20d94d75c42df6af9a36388f41b8526059ca8a7e
Eclipter <Eclipter9000@gmail.com>
2023-03-30 01:55:52 +0300

    Changed max tokens

--- the-gateway-bot.py
+++ the-gateway-bot.py
@@ -7,21 +7,21 @@ from settings import TELEGRAM_BOT_API_KEY, OPENAI_API_KEY
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TELEGRAM_BOT_API_KEY)
-openai.api_key = OPENAI_API_KEY
 dp = Dispatcher(bot)
+openai.api_key = OPENAI_API_KEY
 
 
 async def chat_cmd_handler(message: types.Message):
     """Responds to a chat command by generating a response with OpenAI's GPT-3 model."""
-    prompt_text = message.get_args()
-    model_engine = "text-davinci-003"
     completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt_text,
-        max_tokens=1024,
+        engine="text-davinci-003",
+        prompt=message.get_args(),
+        max_tokens=4096,
         stop=None,
         temperature=0
     )
+
+    await message.answer('Генерирую ответ ...')
     response = completion.choices[0].text
     await message.reply(response)
 
